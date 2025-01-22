bc3d738c21cafe53d8b1d8fc87766eb1abe3e3f7
Kiều Phong BB’m Real <vinhnt@sendo.vn>
2025-01-22 13:29:05 +0700

    feat: add locale text for add certificate page

--- lib/resource/string.dart
+++ lib/resource/string.dart
@@ -39,4 +39,44 @@ abstract class LKey {
 
   static const String pinCodeEncryptDescription = 'pinCodeEncryptDescription';
   static const String securityCode = 'securityCode';
+
+  static const String addLandCertificatePage = 'addLandCertificatePage';
+
+  //Add Land Certificate
+
+  static const String appBarTitle = '$addLandCertificatePage.appBarTitle';
+
+  static const String sectionsLandInfo = '$addLandCertificatePage.sections.landInfo';
+  static const String sectionsLandCertificateImage = '$addLandCertificatePage.sections.landCertificateImage';
+  static const String sectionsAddress = '$addLandCertificatePage.sections.address';
+  static const String sectionsPurchaseDetails = '$addLandCertificatePage.sections.purchaseDetails';
+  static const String sectionsSaleDetails = '$addLandCertificatePage.sections.saleDetails';
+  static const String sectionsLandPlot = '$addLandCertificatePage.sections.landPlot';
+  static const String sectionsArea = '$addLandCertificatePage.sections.area';
+  static const String sectionsTax = '$addLandCertificatePage.sections.tax';
+  static const String sectionsNote = '$addLandCertificatePage.sections.note';
+
+  static const String fieldsNameDescription = '$addLandCertificatePage.fields.nameDescription';
+  static const String fieldsLandCertificateImageTitle = '$addLandCertificatePage.fields.landCertificateImageTitle';
+  static const String fieldsProvinceCity = '$addLandCertificatePage.fields.provinceCity';
+  static const String fieldsDistrict = '$addLandCertificatePage.fields.district';
+  static const String fieldsWard = '$addLandCertificatePage.fields.ward';
+  static const String fieldsSpecificAddress = '$addLandCertificatePage.fields.specificAddress';
+  static const String fieldsPurchaseDate = '$addLandCertificatePage.fields.purchaseDate';
+  static const String fieldsPurchasePrice = '$addLandCertificatePage.fields.purchasePrice';
+  static const String fieldsSaleDate = '$addLandCertificatePage.fields.saleDate';
+  static const String fieldsSalePrice = '$addLandCertificatePage.fields.salePrice';
+  static const String fieldsLandPlotNumber = '$addLandCertificatePage.fields.landPlotNumber';
+  static const String fieldsMapNumber = '$addLandCertificatePage.fields.mapNumber';
+  static const String fieldsAreaSize = '$addLandCertificatePage.fields.areaSize';
+  static const String fieldsUsageForm = '$addLandCertificatePage.fields.usageForm';
+  static const String fieldsUsagePurpose = '$addLandCertificatePage.fields.usagePurpose';
+  static const String fieldsUsageDuration = '$addLandCertificatePage.fields.usageDuration';
+  static const String fieldsResidentialLand = '$addLandCertificatePage.fields.residentialLand';
+  static const String fieldsPerennialTrees = '$addLandCertificatePage.fields.perennialTrees';
+  static const String fieldsTaxRenewalTime = '$addLandCertificatePage.fields.taxRenewalTime';
+  static const String fieldsTaxPaymentDeadline = '$addLandCertificatePage.fields.taxPaymentDeadline';
+  static const String fieldsDetails = '$addLandCertificatePage.fields.details';
+
+  static const String actionsSave = '$addLandCertificatePage.actions.save';
 }
